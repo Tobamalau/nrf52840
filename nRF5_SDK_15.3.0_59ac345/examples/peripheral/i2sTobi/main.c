a3490fc79830585635d64e32a92ae25202a2c719
Tobias Rist <tobias.rist@preh.de>
2019-10-22 15:11:09 +0200

    opus dec not working but implemented

--- nRF5_SDK_15.3.0_59ac345/examples/peripheral/i2sTobi/main.c
+++ nRF5_SDK_15.3.0_59ac345/examples/peripheral/i2sTobi/main.c
@@ -1,79 +1,21 @@
 #include <stdio.h>
 #include <nrf.h>
 #include "sdk_config.h"
-#include "sound.h"
-#include "marioTestenc.c"
+//#include "sound.h"
 #include "opus.h"
+//#include "opusFile.c"
+#include "opusTobi.h"
 
-/*The frame size is hardcoded for this sample code but it doesn't have to be*/
-#define FRAME_SIZE 960
-#define SAMPLE_RATE 48000
-#define OPUSCHANNELS 1
-//#define APPLICATION OPUS_APPLICATION_AUDIO
-#define BITRATE 64000
-#define MAX_FRAME_SIZE (6*960)
-#define MAX_PACKET_SIZE (3*1276)
+/*
+struct opus OpusInstanz = {NULL, NBBYTES, NULL, {}, {}};
+struct frame FrameInstanz = {&OpusInstanz, 0, 0};
 
-int err;
-int frame_size;
-opus_int16 out[MAX_FRAME_SIZE*OPUSCHANNELS];
-unsigned char cbits[MAX_PACKET_SIZE];
-int nbBytes = MAX_PACKET_SIZE;
-unsigned char pcm_bytes[MAX_FRAME_SIZE*OPUSCHANNELS*2];
+volatile int NBbytes[] = {249,134,135,258,189,161,161,161,161,161,161,161,161,161,161,161,161,161,161,161,
+                          104,114,207,175,181,162,184,161,161,161,161,145,177,161,161,157,165,152,137,193,
+                          144,179,154,168,161,161,161,161,161,161,161,137,128,161,148,142,166};
+*/
 
-struct opusfile {
-   OpusDecoder *decoder;
-   int nbBytes;
-   int max_frame_size;
-   int framesize;
-   unsigned char *input;
-   unsigned int inputLenpth;
-   opus_int16 out[MAX_FRAME_SIZE*CHANNELS];
-   //unsigned char cbits[MAX_PACKET_SIZE];
-} OpusInstanz = {NULL, NBBYTES, MAX_FRAME_SIZE, 0, NULL, MAX_PACKET_SIZE, NULL};
 
-void opus()
-{
-   /* Create a new decoder state. */
-   decoder = opus_decoder_create(SAMPLE_RATE, OPUSCHANNELS, &err);
-   if (err<0)
-   {
-      printf("failed to create decoder: %s\n", opus_strerror(err));
-      return;
-   }
-
-    /* Decode the data. In this example, frame_size will be constant because
-       the encoder is using a constant frame size. However, that may not
-       be the case for all encoders, so the decoder must always check
-       the frame size returned. */
-    frame_size = opus_decode(decoder, cbits, nbBytes, out, MAX_FRAME_SIZE, 0);
-    if (frame_size<0)
-    {
-       fprintf(stderr, "decoder failed: %s\n", opus_strerror(frame_size));
-       return;
-    }
-
-    /* Convert to little-endian ordering. */
-    for(int i=0;i<OPUSCHANNELS*frame_size;i++)
-    {
-        pcm_bytes[2*i]=out[i]&0xFF;
-        pcm_bytes[2*i+1]=(out[i]>>8)&0xFF;
-        printf("%o %o\n",  pcm_bytes[2*i], pcm_bytes[2*i+1]);
-
-    }
-    /* Write the decoded audio to file. */
-}
-
-int_fast8_t decodeOpus(struct opusfile *lib)
-{
-   lib->framesize = opus_decode(lib->decoder, lib->input, lib->nbBytes, lib->out, lib->max_frame_size, 0);
-   if (lib->framesize<0)
-   {
-    fprintf(stderr, "decoder failed: %s\n", opus_strerror(lib->framesize));
-    return EXIT_FAILURE;
-   }
-   return EXIT_SUCCESS;
-}
 
 void initI2S()
 {
@@ -102,8 +44,8 @@ void initI2S()
 
    NRF_I2S->ENABLE = 1;
    // Configure data pointer
-   NRF_I2S->TXD.PTR = (uint32_t)&m_musik_table[0];
-   NRF_I2S->RXTXD.MAXCNT = leftChannelSize;//sizeof(sine_table) / sizeof(uint32_t);//
+   //NRF_I2S->TXD.PTR = (uint32_t)&m_musik_table[0];
+   //NRF_I2S->RXTXD.MAXCNT = leftChannelSize;//sizeof(sine_table) / sizeof(uint32_t);//
    //printf("NRF_I2S->RXTXD.MAXCNT:%d,%d,%d", NRF_I2S->RXTXD.MAXCNT,  sizeof(sine_table), sizeof(uint32_t));
 }
 
@@ -113,34 +55,55 @@ int main(void)
 {
    printf("Start");
    initI2S();
+
+/*
+   uint_fast16_t loopcnt = 0;
+   uint_fast16_t nbbytessum = 0;
+   uint_fast16_t len = sizeof(NBbytes) / sizeof(NBbytes[0]);
+   initOpus(&OpusInstanz);
+   FrameInstanz.opus_t = &OpusInstanz;
+  */
+   uint_fast16_t len = 57;
+   volatile uint_fast8_t newFrame = 0;
+   struct opus OpusInstanz = {NULL, NBBYTES, NULL, {}, {}};
+   struct frame FrameInstanz = {&OpusInstanz, 0, 0};
+
+   getPcm(&FrameInstanz);
+   NRF_I2S->TXD.PTR = (uint32_t)OpusInstanz.pcm_bytes;
+   NRF_I2S->RXTXD.MAXCNT = NBbytes[FrameInstanz.loopcnt-1];
    // Start transmitting I2S data
    NRF_I2S->TASKS_START = 1;
 
-   /* Create a new decoder state. */
-   OpusInstanz.decoder = opus_decoder_create(SAMPLE_RATE, OPUSCHANNELS, &err);
-   if (err<0)
-   {
-      printf("failed to create decoder: %s\n", opus_strerror(err));
-      return 0;
-   }
-   OpusInstanz.input = &marioTestenc_opus;
-   decodeOpus(&OpusInstanz);
-   unsigned char pcm_bytes[MAX_FRAME_SIZE*CHANNELS*2];
-   /* Convert to little-endian ordering. */
-   for(int i=0;i<CHANNELS*OpusInstanz.framesize;i++)
-   {
-      pcm_bytes[2*i]=OpusInstanz.out[i]&0xFF;
-      pcm_bytes[2*i+1]=(OpusInstanz.out[i]>>8)&0xFF;
-   }
 
-  // Since we are not updating the TXD pointer, the sine wave will play over and over again.
-  // The TXD pointer can be updated after the EVENTS_TXPTRUPD arrives.
-  while (1)
-  {
+   // Since we are not updating the TXD pointer, the sine wave will play over and over again.
+   // The TXD pointer can be updated after the EVENTS_TXPTRUPD arrives.
+   while (1)
+   {
 
     __WFE();
-    
-  }
-
-  opus_decoder_destroy(OpusInstanz.decoder);
+    while (len>FrameInstanz.loopcnt)
+    {
+       /*
+       OpusInstanz.input = marioTestenc_opus + nbbytessum;
+       OpusInstanz.nbBytes = NBbytes[loopcnt];
+       decodeOpusFrame(&OpusInstanz);
+       nbbytessum += OpusInstanz.nbBytes;
+       loopcnt++;*/
+       if(newFrame)
+       {
+          getPcm(&FrameInstanz);
+          printf("pcm:%o", OpusInstanz.pcm_bytes[0]);
+       }
+       /* New I2S buffer*/
+       if(NRF_I2S->EVENTS_TXPTRUPD  != 0)
+       {
+           NRF_I2S->TXD.PTR = (uint32_t)OpusInstanz.pcm_bytes;
+           NRF_I2S->RXTXD.MAXCNT = NBbytes[FrameInstanz.loopcnt-1];
+           NRF_I2S->EVENTS_TXPTRUPD = 0;
+           newFrame = 1;
+       }
+    }
+    FrameInstanz.loopcnt = 0;
+    FrameInstanz.nbbytessum = 0;
+   }
 }
